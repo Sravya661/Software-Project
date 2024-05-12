e0bda488689a33bbfbb92117a4bacf31fff66823
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-12 22:43:15 +0530

    added few features

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CarouselModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  items$!: Observable<any[]>;
+  data = [];
+
+  constructor(
+    private http: HttpClient
+  ){ }
+
+
+  ngOnInit(): void {
+    
+    // this.items$ = [];
+    // Fetch data from your API (replace 'apiUrl' with your backend endpoint)
+    // this.items$ = this.http.get<any[]>('apiUrl').pipe(
+    //   map(response => response.data) // Assuming your API response contains an array of data
+    // );
+  }
 }
