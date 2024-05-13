21f946f117d348bf5edba0f7b54f071b1d1710a2
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-13 08:17:27 +0530

    nothing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { PopupService } from '../popup.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -19,18 +21,29 @@ export class HomeComponent {
 
   items$!: Observable<any[]>;
   data = [];
+  userEmail = '';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private popUpService : PopupService,
+    private snackBar : MatSnackBar
   ){ }
 
 
   ngOnInit(): void {
-    
+    this.userEmail = 'choudhary98akash@gmal.com'
     // this.items$ = [];
     // Fetch data from your API (replace 'apiUrl' with your backend endpoint)
     // this.items$ = this.http.get<any[]>('apiUrl').pipe(
     //   map(response => response.data) // Assuming your API response contains an array of data
     // );
+  }
+
+  logout() {
+    const snackBarRef = this.snackBar.open('Are you sure you want to logout?', 'Yes', {
+      duration: 5000 });
+    snackBarRef.onAction().subscribe(() => {
+      console.log('User confirmed logout. Implement logout logic here.');
+    });
   }
 }
